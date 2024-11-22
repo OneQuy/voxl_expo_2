5111711bb0be9bd7cd9944819402d46103058c16
Minh Quy <onequy@gmail.com>
2024-11-22 16:26:44 +0700

    sample_scene

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -40,6 +40,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="sample_scene"
+        options={{
+          title: '3D',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="move.3d" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
--- app/(tabs)/sample_scene.tsx
+++ app/(tabs)/sample_scene.tsx
@@ -2,17 +2,22 @@ import React, { useRef, useEffect } from "react";
 import { View } from "react-native";
 import { GLView } from "expo-gl";
 import { Renderer } from "expo-three";
+
+// @ts-ignore
 import * as THREE from "three";
 
-export default function SampleThreeScene() {
+export default function sample_scene() {
+    // @ts-ignore
     const onContextCreate = async (gl) => {
         // Create a WebGLRenderer
         const renderer = new Renderer({ gl });
+        
+        // @ts-ignore
         renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
-
+        
         // Create a Scene
         const scene = new THREE.Scene();
-
+        
         // Add a Camera
         const camera = new THREE.PerspectiveCamera(
             75,
@@ -21,24 +26,25 @@ export default function SampleThreeScene() {
             1000
         );
         camera.position.z = 5;
-
+        
         // Add a Cube
         const geometry = new THREE.BoxGeometry();
         const material = new THREE.MeshBasicMaterial({ color: 'red' });
         const cube = new THREE.Mesh(geometry, material);
         scene.add(cube);
-
+        
         // Animate the Cube
         const animate = () => {
             requestAnimationFrame(animate);
-
+            
             cube.rotation.x += 0.01;
             cube.rotation.y += 0.01;
-
+            
+            // @ts-ignore
             renderer.render(scene, camera);
             gl.endFrameEXP();
         };
-
+        
         animate();
     };
 
