ef087a26b1b44101ac360db700063fb869af1866
Minh Quy <onequy@gmail.com>
2024-11-22 16:47:50 +0700

    tomato cude

--- app/(tabs)/test3Dscene.tsx
+++ app/(tabs)/test3Dscene.tsx
@@ -8,45 +8,43 @@ import * as THREE from "three";
 
 export default function test3Dscene() {
     // @ts-ignore
-    const onContextCreate = async (gl) => {
-        // Create a WebGLRenderer
+    const onContextCreate = (gl) => {
         const renderer = new Renderer({ gl });
-        
+
         // @ts-ignore
         renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
-        
-        // Create a Scene
+      
         const scene = new THREE.Scene();
-        
-        // Add a Camera
+      
         const camera = new THREE.PerspectiveCamera(
-            75,
-            gl.drawingBufferWidth / gl.drawingBufferHeight,
-            0.1,
-            1000
+          75,
+          gl.drawingBufferWidth / gl.drawingBufferHeight,
+          0.1,
+          1000
         );
         camera.position.z = 5;
-        
-        // Add a Cube
+      
         const geometry = new THREE.BoxGeometry();
-        const material = new THREE.MeshBasicMaterial({ color: 'red' });
+        const material = new THREE.MeshBasicMaterial({ color: 'tomato' });
         const cube = new THREE.Mesh(geometry, material);
         scene.add(cube);
-        
-        // Animate the Cube
+      
         const animate = () => {
-            requestAnimationFrame(animate);
-            
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
-            
-            // @ts-ignore
-            renderer.render(scene, camera);
-            gl.endFrameEXP();
+          requestAnimationFrame(animate);
+      
+          const offset = 0.05;
+
+          cube.rotation.x += offset
+          cube.rotation.y += offset
+      
+          // @ts-ignore
+          renderer.render(scene, camera);
+          gl.endFrameEXP();
         };
-        
+      
         animate();
-    };
+      };
+      
 
     return (
         <View style={{ flex: 1 }}>
